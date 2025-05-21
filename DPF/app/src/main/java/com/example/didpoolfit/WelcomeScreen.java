165eb222dd8354429cc6d968122c2acdd8d809b1
SCPSOSATB <165580413+SCPSOSATB@users.noreply.github.com>
2025-05-21 18:27:31 +0600

    Add activity_welcome_screen.xml Add color gradient Add welcome text

---
+++ DPF/app/src/main/java/com/example/didpoolfit/WelcomeScreen.java
@@ -0,0 +1,31 @@
+package com.example.didpoolfit;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WelcomeScreen extends AppCompatActivity {
+
+    TextView tV_WT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        int[] color = {Color.parseColor("#CC8FED"), Color.parseColor("#6B50F6")};
+        float[] position = {0, 1};
+        Shader.TileMode tile_mode = Shader.TileMode.REPEAT;
+        Shader shader_gradient = new LinearGradient(0, 0, 0, 35, color, position, tile_mode);
+        tV_WT = findViewById(R.id.welcomeText);
+        tV_WT.getPaint().setShader(shader_gradient);
+    }
+}
