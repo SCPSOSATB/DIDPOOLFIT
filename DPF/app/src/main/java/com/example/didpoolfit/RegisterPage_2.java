02e52e7e2c5a84374e1b096562e6eb122d87eb00
SCPSOSATB <165580413+SCPSOSATB@users.noreply.github.com>
2025-05-29 19:28:16 +0600

    micro fix
    add:
    RegisterPage_2
    RegisterPage_3_1
    grad_bt_k.xml

---
+++ DPF/app/src/main/java/com/example/didpoolfit/RegisterPage_2.java
@@ -0,0 +1,31 @@
+package com.example.didpoolfit;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class RegisterPage_2 extends AppCompatActivity {
+
+    TextView tV_CT;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_page2);
+
+        int[] color = {Color.parseColor("#CC8FED"), Color.parseColor("#6B50F6")};
+        float[] position = {0, 1};
+        Shader.TileMode tile_mode = Shader.TileMode.REPEAT;
+        Shader shader_gradient = new LinearGradient(0, 0, 0, 35, color, position, tile_mode);
+        tV_CT = findViewById(R.id.completeText);
+        tV_CT.getPaint().setShader(shader_gradient);
+    }
+}
